2e0788aaafef264fb837ae43248991a7141a3e8a
dinnozap <dinnozap@gmail.com>
2017-10-10 16:57:28 +0200

    Installer

---
+++ InstallIWO.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo  "Welcome to the IntraWebOS installer! "
+echo "The main folder will be located at $u/home/Desktop/IntraWebOSMain"
+echo "Please run the script in sudo, type : "sudo bash StartIWO.sh" to do so"
+echo "Do not run this installer if IntraWebOS is already installed, it may lead to bugs"
+sleep 10s
+cd Desktop
+mkdir IntraWebOSMain
+cd IntraWebOSMain
+wget 
