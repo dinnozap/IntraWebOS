2b9621302d6e0e187e9b0c719c3fa93650dba2e3
dinnozap <dinnozap@gmail.com>
2017-10-10 18:08:04 +0200

    Update InstallIWO.sh

--- InstallIWO.sh
+++ InstallIWO.sh
@@ -14,14 +14,22 @@ CYAN="\\033[1;36m"
 
 echo  "Welcome to the IntraWebOS installer! "
 echo  "You need to be connected to ine Internet to Install IntraWebOS"
-echo "The main folder will be located at $u/home/Desktop/IntraWebOSMain"
 echo -e "Please run the script in sudo, type : $VLEU sudo bash StartIWO.sh $NORMAL to do so"
 echo "Do not run this installer if IntraWebOS is already installed, it may lead to bugs"
 sleep 10s
 clear
-cd Desktop
 mkdir IntraWebOSMain
 cd IntraWebOSMain
 mkdir Extentions
+cd ..
 echo "Installing..."
-wget https://raw.githubusercontent.com/dinnozap/IntraWebOS/master/IWOMain/Menu.sh
+wget -q https://raw.githubusercontent.com/dinnozap/IntraWebOS/master/IWOMain/Menu.sh
+read -p 'Done! do you want to launch the Menu[Y/n]' lMenu
+if [ $lMenu == "Y" ]
+then
+	sh Menu.sh
+elif [ $lMenu == "n" ]
+then
+	exit
+else 
+echo "Please use an appropriate command"
